b6c0e595a6e7b15fa49b9063f31a0592b3ec3a82
Lob2018 <@>
2023-01-22 16:50:03 +0100

    Fix : Rental creation needs  : Body > form-data > picture as file

--- src/main/java/fr/hatclic/chatop/controller/RentalsController.java
+++ src/main/java/fr/hatclic/chatop/controller/RentalsController.java
@@ -12,8 +12,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import fr.hatclic.chatop.model.Rentals;
 import fr.hatclic.chatop.service.RentalsService;
@@ -23,7 +25,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 @RestController
 @RequestMapping("/api/rentals")
 @SecurityRequirement(name = "bearerAuth")
-@Tag(name = "rentals", description = "The rentals API. Contains all the operations that can be performed on a rental.")
+@Tag(name = "Rentals", description = "The rentals API. Contains all the operations that can be performed on a rental.")
 public class RentalsController {
 
 	@Autowired
@@ -42,11 +44,19 @@ public class RentalsController {
 
 	@PostMapping("")
 	@ResponseBody
-	public HashMap<String, String> create(@ModelAttribute Rentals rental) {
+	public HashMap<String, String> create(@RequestParam("picture") MultipartFile picture,
+			@RequestParam("name") String name, @RequestParam("surface") double surface,
+			@RequestParam("description") String description, @RequestParam("owner_id") Long owner_id) {
+		Rentals rental = new Rentals();
 		final HashMap<String, String> map = new HashMap<>();
 		try {
+			rental.setName(name);
+			rental.setSurface(surface);
+			rental.setDescription(description);
+			rental.setOwner_id(owner_id);
 			rental.setCreated_at(ZonedDateTime.now());
 			rental.setUpdated_at(ZonedDateTime.now());
+			rental.setPicture(picture.getOriginalFilename());
 			rentalsService.createRental(rental);
 			map.put("token", "jwt");
 			return map;
